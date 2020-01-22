3d75357860ecf7e0197fcf8c26891b67d0f64f1c
Shiba <beast14425@gmail.com>
2020-01-22 18:27:14 -0500

    HUMBLE BEGINNINGS
    Here we create the base code of Miku: the robot we're building for the 2020 season! This code is heavily under development!

--- src/main/java/frc/robot/commands/RunHarvester.java
+++ src/main/java/frc/robot/commands/RunHarvester.java
@@ -46,6 +46,7 @@ public class RunHarvester extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    armExtended = false;
   }
 
   // Returns true when the command should end.
